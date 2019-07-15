79db12a07e93d0eadadb3641abe5442d7e712587
suoyue <suoyue@yunlizhihui.com>
2019-07-15 14:45:50 +0800

    init

---
+++ precache-manifest.214eb626a8202dca73920d58a4c08a59.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e99268f6006d05346492798a8968cef",
+    "url": "/images/3.9e99268f.jpg"
+  },
+  {
+    "revision": "c1393e7f1e78e1778a878acafc34f969",
+    "url": "/images/4.c1393e7f.jpg"
+  },
+  {
+    "revision": "3898dbee16d9f906da5a57060e7810bf",
+    "url": "/images/6.3898dbee.jpg"
+  },
+  {
+    "revision": "ea23dd84dfd0f581d8554238970b63d0",
+    "url": "/images/a.ea23dd84.jpg"
+  },
+  {
+    "revision": "360f80c99a204905158714d9f0cdf320",
+    "url": "/images/b.360f80c9.jpg"
+  },
+  {
+    "revision": "3fb818ce15d28fe25d2f8627842e340d",
+    "url": "/images/bg.3fb818ce.gif"
+  },
+  {
+    "revision": "32d662138116dfee0d326593459e40da",
+    "url": "/images/c.32d66213.jpg"
+  },
+  {
+    "revision": "e4fdcb29a4d3683e953e63a4c3c0a410",
+    "url": "/images/chun.e4fdcb29.jpg"
+  },
+  {
+    "revision": "d1f90fe0201b38936f90b48a466fdff0",
+    "url": "/images/dong.d1f90fe0.jpg"
+  },
+  {
+    "revision": "ca847c7cb69d98e8789fb13bcb37f297",
+    "url": "/images/flower-left-2.ca847c7c.png"
+  },
+  {
+    "revision": "2bb0deeeab4d4c4a6e2e38cd85bed0d9",
+    "url": "/images/flower-right-2.2bb0deee.png"
+  },
+  {
+    "revision": "67f8faacb86d49776a05a6b8cc773a93",
+    "url": "/images/line.67f8faac.png"
+  },
+  {
+    "revision": "0acc854c4058c5a45187c07babcefa32",
+    "url": "/images/qiu.0acc854c.jpg"
+  },
+  {
+    "revision": "9a939988973667dc15a959ad59b3232c",
+    "url": "/images/test.9a939988.jpeg"
+  },
+  {
+    "revision": "e69ce92a6a0c11d6335584a2d23917a5",
+    "url": "/images/xia.e69ce92a.jpg"
+  },
+  {
+    "revision": "3b5612cce19b0427f828cc3c566bc1df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d21f5814cc03dd9282ec7ce131df0911",
+    "url": "/music/星星.mp3"
+  },
+  {
+    "revision": "607118966a05d9018bb1",
+    "url": "/static/css/2.e22dc143.chunk.css"
+  },
+  {
+    "revision": "bc8ace17b17697563e76",
+    "url": "/static/css/main.af55047a.chunk.css"
+  },
+  {
+    "revision": "607118966a05d9018bb1",
+    "url": "/static/js/2.7df7953d.chunk.js"
+  },
+  {
+    "revision": "bc8ace17b17697563e76",
+    "url": "/static/js/main.f391b720.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
